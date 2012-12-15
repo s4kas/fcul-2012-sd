5e155b2bc9cd99dce1047e2f2c2bf00594c240fb
brunofgmarques@gmail.com <brunofgmarques@gmail.com@9240f830-19d0-5a1c-ca44-9a8c5c8e660b>
2012-12-15 22:31:03 +0000

    [BM] connection mudada para common

---
+++ branches/entrega1/fcul-2012-sd/src/org/sd/common/connection/IConnection.java
@@ -0,0 +1,16 @@
+package org.sd.common.connection;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import org.sd.common.messages.IMessage;
+
+public interface IConnection {
+	
+	public IMessage getMessage();
+	public void setMessage(IMessage message);
+	public Socket getSocket();
+	public ObjectOutputStream getOutputStream();
+	public ObjectInputStream getInputStream();
+}
