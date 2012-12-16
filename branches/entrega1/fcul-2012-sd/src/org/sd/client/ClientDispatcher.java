11df1bf59a802ee30b9ef9baf0969e1cfdb1bcae
brunofgmarques@gmail.com <brunofgmarques@gmail.com@9240f830-19d0-5a1c-ca44-9a8c5c8e660b>
2012-12-16 22:20:15 +0000

    [BM] client

---
+++ branches/entrega1/fcul-2012-sd/src/org/sd/client/ClientDispatcher.java
@@ -0,0 +1,20 @@
+package org.sd.client;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class ClientDispatcher implements Observer {
+
+	//C_RCV_SL - Processa a recepcao da lista de servidores
+		//A_RCV_RDT - Processa a ordem de redirecionamento.
+		//S_C_RCV_AAD - Processa a resposta a um pedido AAD previo.
+		//A_RCV_AG - Processa a recepção de uma AGenda
+		//S_C_RCV_HS - Processa a aceitação de HS dum servidor.
+
+	public void update(Observable obs, Object obj) {
+		
+	}
+	
+	
+	
+}
