fe1399bf6684a0e3bea01e3793ae6ae9f6e422ec
brunofgmarques <brunofgmarques@gmail.com>
2012-12-16 12:20:52 +0000

    [BM] cliente e properties

--- fcul-2012-sd/src/org/sd/client/ClientConfig.java
+++ fcul-2012-sd/src/org/sd/client/ClientConfig.java
@@ -5,14 +5,18 @@ import org.sd.common.properties.PropertiesManager;
 
 public class ClientConfig implements IConfig {
 	
-	private String clientAddress;
-	private int clientPort;
+	private String[] clientAddress;
+	private int[] clientPort;
 	private int connectionTimeout;
 	
 	public boolean loadConfig() {
 		try {
-			clientAddress = PropertiesManager.getClientProps(false).getProperty("clientAddress");
-			clientPort = Integer.parseInt(PropertiesManager.getClientProps(false).getProperty("clientPort"));
+			clientAddress = PropertiesManager.getClientProps(false).getProperty("clientAddress").split(","); 
+			String[] ports = PropertiesManager.getClientProps(false).getProperty("clientPort").split(",");
+			clientPort = new int[ports.length];
+			for (int i = 0 ; i < ports.length; i ++) {
+				clientPort[i] = Integer.parseInt(ports[i]);
+			}
 			connectionTimeout = Integer.parseInt(PropertiesManager.getClientProps(false).getProperty("connectionTimeout"));
 		} catch (Exception e) {
 			//TODO BM do something
@@ -22,11 +26,11 @@ public class ClientConfig implements IConfig {
 		return true;
 	}
 
-	public String getClientAddress() {
+	public String[] getClientAddress() {
 		return clientAddress;
 	}
 
-	public int getClientPort() {
+	public int[] getClientPort() {
 		return clientPort;
 	}
 	
